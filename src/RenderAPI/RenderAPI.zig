6c449e2666fa5b171f9c6c6f2c663d969f553bd4
Malcolm Parry <malckinator@gmail.com>
2025-06-29 21:43:38 +0000

    added more things to renderapi.zig

--- src/RenderAPI/RenderAPI.zig
+++ src/RenderAPI/RenderAPI.zig
@@ -1,3 +1,6 @@
+pub const Vulkan = @import("Vulkan.zig");
+
 pub const Instance = @import("Instance.zig");
 pub const Device = @import("Device.zig");
 pub const Display = @import("Display.zig");
+pub const RenderPass = @import("RenderPass.zig");
