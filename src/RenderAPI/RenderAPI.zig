6379cf60adc51c6d3fdee6ac51e3c6ca7f641d44
Malcolm Parry <malckinator@gmail.com>
2025-09-15 22:38:16 +0100

    added shader to RenderAPI.zig

--- src/RenderAPI/RenderAPI.zig
+++ src/RenderAPI/RenderAPI.zig
@@ -5,3 +5,4 @@ pub const Device = @import("Device.zig");
 pub const Display = @import("Display.zig");
 pub const RenderPass = @import("RenderPass.zig");
 pub const Buffer = @import("Buffer.zig");
+pub const Shader = @import("Shader.zig");
