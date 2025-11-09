d3b8e2ab3af8be2dc28532c60924bee421d94083
Malcolm Parry <malckinator@gmail.com>
2025-11-09 19:29:39 +0000

    added waiting on many fences and checking fence status

--- src/gpu/wait_objects.zig
+++ src/gpu/wait_objects.zig
@@ -18,6 +18,10 @@ pub const Semaphore = struct {
         const vk_alloc: ?*vk.AllocationCallbacks = null;
         device._device.destroySemaphore(this._semaphore, vk_alloc);
     }
+
+    pub fn _nativesFromSlice(these: []@This()) []vk.Semaphore {
+        return @ptrCast(these);
+    }
 };
 
 pub const Fence = struct {
@@ -43,12 +47,31 @@ pub const Fence = struct {
         try device._device.resetFences(1, @ptrCast(&this._fence));
     }
 
-    pub fn wait(this: *@This(), device: *Device, timeout_ns: ?u64) !void {
-        const wait_all: vk.Bool32 = .true;
-        return switch (try device._device.waitForFences(1, @ptrCast(&this._fence), wait_all, timeout_ns orelse std.math.maxInt(u64))) {
-            .success => {},
-            .timeout => error.Timeout,
+    pub const WaitForEnum = enum { single, all };
+    pub const WaitForResult = enum { success, timeout };
+
+    pub fn waitMany(these: []@This(), device: *Device, how_many: WaitForEnum, timeout_ns: ?u64) !WaitForResult {
+        const natives = _nativesFromSlice(these);
+        return switch (try device._device.waitForFences(natives.len, natives.ptr, how_many == .all, timeout_ns orelse std.math.maxInt(u64))) {
+            .success => .success,
+            .timeout => .timeout,
             else => unreachable,
         };
+    }
+
+    pub fn wait(this: @This(), device: *Device, how_many: WaitForEnum, timeout_ns: ?u64) !void {
+        return waitMany(&.{this}, device, how_many, timeout_ns);
+    }
+
+    pub fn checkSignaled(this: @This(), device: *Device) bool {
+        return switch (try device._device.getFenceStatus(this._fence)) {
+            .success => true,
+            .not_ready => false,
+            else => unreachable,
+        };
+    }
+
+    pub fn _nativesFromSlice(these: []@This()) []vk.Fence {
+        return @ptrCast(these);
     }
 };
